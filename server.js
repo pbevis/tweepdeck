b359bb3e58a6c5dfc970e8da7ade4f28038602cd
pbevis <pbevis@hotmail.com>
2013-10-02 14:06:13 +0100

    Added Twitter class to encapsulate the logic of communicating with Twitter.

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 var express = require('express'),
     passport = require('passport'),
-    https = require('https'),
-    url = require('url'),
-    querystring = require('querystring'),
-    crypto = require('crypto'),
-    util = require('util'),
     TwitterStrategy = require('passport-twitter').Strategy,
-    OAuth = require('./oauth').OAuth,
+    Twitter = require('./lib/twitter').Twitter
     app = express();
     
 var CONSUMER_KEY = 'tmIkEIh5Rjo4Fgr9iQJw',
@@ -57,147 +52,16 @@ app.get('/auth/twitter/callback',
                                      failureRedirect: '/login' }));
                                     
 app.get('/followers/:id', function(req, res) {
-
-    var oAuth = new OAuth({
+    var twitter = new Twitter({
         consumerKey: CONSUMER_KEY, 
         consumerSecret: CONSUMER_SECRET,
         token: req.user.token,
         tokenSecret: req.user.tokenSecret
     });
-  
-    // Get a list of follower ids.
-    var url = 'https://api.twitter.com/1.1/followers/ids.json?cursor=-1&screen_name=' + req.params.id + '&count=100&stringify_ids=true';  
-    oAuth.request({ url: url }, function (response) {
-        console.log("Got response: " + response.statusCode);
-        var data = ''
-        response.on('data', function (chunk) {
-            data += chunk;
-        });
-        response.on('end', function () {
-            var result = JSON.parse(data);
-            var body = encodeURIComponent('user_id') + '=' + encodeURIComponent(result.ids.join(','));
-            oAuth.request({ 
-                url: 'https://api.twitter.com/1.1/users/lookup.json?include_entities=false',
-                method: 'POST',
-                body: body
-            }, function (response) {
-                console.log("Got response: " + response.statusCode);
-                var data = ''
-                response.on('data', function (chunk) {
-                    data += chunk;
-                });             
-                response.on('end', function () {
-                    res.send(JSON.parse(data));
-                });
-            }).end();
-        });
-    }).end();
-  
-  //res.send(url.parse('https://api.twitter.com/1.1/followers/ids.json?cursor=-1&screen_name=pbevis&count=5000'));
-  
-  /*var config = {
-    url: 'https://api.twitter.com/1.1/followers/ids.json?cursor=-1&screen_name=pbevis&count=5000',
-    method: 'GET',
-    consumerKey: CONSUMER_KEY,
-    signature: null,
-    signatureMethod: 'HMAC-SHA1',
-    token: req.user.token,
-    //token: '80892654-KCx7LTHCaVcp13NjeGEkSvOge3QIdweKaOqb3TzpH',
-    timestamp: Math.floor(new Date().getTime()/1000),
-    version: "1.0",
-    consumerSecret: CONSUMER_SECRET,
-    tokenSecret: req.user.tokenSecret
-    //tokenSecret: 'VkAwOYPPJTjnYwsVCQuped9lmSnO04ld0YocVxWvg'
-  };
-  
-  var nonce = [];
-  var chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9'];
-  var pos;  
-  for (var i = 0; i < 32; i++) {
-    pos = Math.floor(Math.random() * chars.length);
-    nonce[i]=  chars[pos];
-  }
-  config.nonce = nonce.join('');
-  
-  //console.log(oAuth);
-  config.nonce = oAuth.generateNonce(32);
-  
-  var parts = url.parse(config.url);
-  dictionary = querystring.parse(parts.query);
-  dictionary.oauth_consumer_key = config.consumerKey;
-  dictionary.oauth_nonce = config.nonce;
-  dictionary.oauth_signature_method = config.signatureMethod;
-  dictionary.oauth_timestamp = config.timestamp;
-  dictionary.oauth_token = config.token;
-  dictionary.oauth_version = config.version;
-  
-  // Convert the dictionary to an array of key/value pairs so it can be sorted.
-  var array = [];
-  for (var key in dictionary) {
-    if (dictionary.hasOwnProperty(key)) {
-      array.push({ key: encodeURIComponent(key), value: encodeURIComponent(dictionary[key]) });
-    }
-  };
-  
-  // Sort the pairs by key and then by value. 
-  array.sort(function(a,b) {
-    var result = a.key.localeCompare(b.key);
-    if (result === 0) {
-      a.value.localeCompare(b.value);
-    }
-    return result
-  });
-  
-  // Build the parameters string by concatinating the key/value pairs.
-  var parameterString = '';
-  array.forEach(function (element, index) {
-    parameterString = parameterString + element.key + '=' + element.value + '&';
-  });  
-  parameterString = parameterString.substring(0, parameterString.length - 1);
-
-  // Prepend the protocol and the URL (minus the query string).
-  signatureString = encodeURIComponent(parts.href.substring(0, parts.href.indexOf(parts.search))) + '&' + encodeURIComponent(parameterString);
-  signatureString = config.method.toUpperCase() + '&' + signatureString;
-  
-  // Build the signing key from the consumer secret the token secret.
-  signingKey = encodeURIComponent(config.consumerSecret) + '&' + encodeURIComponent(config.tokenSecret);
-  
-  // Create the signature hash from the signature string and the signing key.
-  config.signature = encodeURIComponent(crypto.createHmac('sha1', signingKey).update(signatureString).digest('base64'));
-  
-  var oAuth = 'OAuth oauth_consumer_key="' + config.consumerKey + '"';
-  oAuth = oAuth + ', oauth_nonce="' + config.nonce + '"';
-  oAuth = oAuth + ', oauth_signature="' + config.signature + '"';
-  oAuth = oAuth + ', oauth_signature_method="' + config.signatureMethod + '"';
-  oAuth = oAuth + ', oauth_timestamp="' + config.timestamp + '"';
-  oAuth = oAuth + ', oauth_token="' + config.token + '"';
-  oAuth = oAuth + ', oauth_version="' + config.version + '"';
-   
-  var options = {
-    host: 'api.twitter.com',
-    method: config.method,
-    path: '/1.1/followers/ids.json?cursor=-1&screen_name=pbevis&count=5000',
-    headers: {
-      'Connection': 'close',
-      'User-Agent': 'TweepDeck',
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': oAuth
-    }
-  };
-  
-  var request = https.request(options, function (response) {
-    console.log("Got response: " + response.statusCode);
-    var data = ''
-    response.on('data', function (chunk) {
-      data += chunk;
-    });
-    response.on('end', function () {
-      res.send(JSON.parse(data));
-    });
-  });
-  request.end();*/
     
-  //res.send('Followers of ' + req.user.displayName + ' (@' + req.user.username + ')');
+    twitter.getFollowers(req.params.id, function (result) {
+        res.send(result);
+    });
 });
 
 // Start the app by listening on <port>
